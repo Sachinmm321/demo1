95206985e91a976273991a5925956b0257ae70f7
sachin <support@sachin.co>
2025-11-09 16:36:40 +0530

    Third commit of Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
 	public static void main(string[] args) {
-	System.out.println("Welome to Git's Online Training!);
+	System.out.println("Welome to Git's Online Training!");
+	System.out.println("Welcome to Coaching center!")
 	}
 }
