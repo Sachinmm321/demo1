ac93d36f865f8038bde42287aa3fe14beeba91db
Sachinmm321 <mangijisachin1234567@gmail.com>
2025-11-13 15:39:08 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,5 @@
 public class Hello{
 	public static void main(String[] args) {
-	System.out.println("Welcome to Git's Online Training!");
-	System.out.println("Welcome to Coaching center!");
+	System.out.println("Welcome to Jenkins online Training!");
 	}
 }
